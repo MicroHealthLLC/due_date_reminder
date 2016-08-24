c25a618da261ae60e4a9ad6e09dfeabbdda36ac1
Jean Bertrand <jbertrand@siliconsalad.com>
2016-08-24 16:59:46 +0200

    Send mail only for Issue with custom field "Rappel par mail" set to true

--- app/models/reminder_mailer.rb
+++ app/models/reminder_mailer.rb
@@ -49,9 +49,17 @@ class ReminderMailer < Mailer
   private
 
   def self.insert(data, issue)
+    return unless check_custom_value(issue)
     data[issue.assigned_to] ||= {}
     data[issue.assigned_to][issue.project] ||= []
     data[issue.assigned_to][issue.project] << issue
   end
 
+  def self.check_custom_value(issue)
+    return CustomValue.find_or_initialize_by(custom_field_id: id_custom_field, customized_id: issue.id).value == "1"
+  end
+
+  def self.id_custom_field
+    @id_custom_field ||= CustomField.find_by(name: "Rappel par mail").id
+  end
 end
